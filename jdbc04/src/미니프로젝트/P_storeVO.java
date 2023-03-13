b196d1cebce02bb7c9aeab05534179f8b233506b
shinbeomsik <124110586+shinbeomsik@users.noreply.github.com>
2023-03-13 17:59:27 +0900

    day27 2023/03/13

---
+++ jdbc04/src/미니프로젝트/P_storeVO.java
@@ -0,0 +1,31 @@
+package 미니프로젝트;
+
+public class P_storeVO {
+
+		private String name;
+		private String category;
+		private double rating;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getCategory() {
+			return category;
+		}
+		public void setCategory(String category) {
+			this.category = category;
+		}
+		public double getRating() {
+			return rating;
+		}
+		public void setRating(double rating) {
+			this.rating = rating;
+		}
+	
+		
+		
+	
+
+}
